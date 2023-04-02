6cd8b78c4556194c205572ffcbea293742d3eecc
ngnmsn <ngnmsn.dev@gmail.com>
2023-04-03 01:52:37 +0900

    Create Top Screen

---
+++ src/pages/Top.tsx
@@ -0,0 +1,17 @@
+import SiteTitle from '../components/SiteTitle';
+
+function Top() {
+  return (
+    <div className='w-full'>
+      <div className='w-full mt-24'>
+        <SiteTitle />
+      </div>
+      <div className='w-full pt-12'>
+        <button className='w-[5rem] h-[2rem] mx-4 rounded-lg bg-green-600 text-white'><p className='text-xl'>Login</p></button>
+        <button className='w-[5rem] h-[2rem] mx-4 rounded-lg bg-green-600 text-white'><p className='text-xl'>Signup</p></button>
+      </div>
+    </div>
+  )
+}
+
+export default Top;
