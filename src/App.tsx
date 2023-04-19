68e54989122e4308e2bb25718ad69c59faa89fc5
ngnmsn <ngnmsn.dev@gmail.com>
2023-04-20 01:51:03 +0900

    Modify routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Top from './pages/Top';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { supabase } from './supabaseClient';
+import Login from './pages/Login';
+import Home from './pages/Home';
 import Group from './pages/Group';
 import TalkSession from './pages/TalkSession';
 import SecretWordSetting from './pages/SecretWordSetting';
@@ -13,11 +15,32 @@ import TalkSessionEdit from './pages/TalkSessionEdit';
 import './App.css';
 
 function App() {
+  const [session, setSession] = useState<any>(null);
+  const [userId, setUserId] = useState<string|null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+      if (session!=null){
+        setUserId(session.user.id)
+      }
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+      if (session!=null){
+        setUserId(session.user.id)
+      }
+    })
+
+  }, [session, userId])
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Top />}></Route>
+          <Route path='/' element={session ? <Navigate replace to='/home'/> : <Login />}></Route>
+          <Route path='/home' element={session ? <Home userId={userId}/> : <Navigate replace to='/'/>}></Route>
           <Route path='/group' element={<Group />}></Route>
           <Route path='/talk_session' element={<TalkSession />}></Route>
           <Route path='/secret_word_setting' element={<SecretWordSetting />}></Route>
